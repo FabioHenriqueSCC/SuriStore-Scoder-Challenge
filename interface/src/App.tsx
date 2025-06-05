5eb4b6b0eb1305a2e46420cb578b22d406682ced
FabioHenriqueSCC <fhscc7@gmail.com>
2025-06-05 00:39:27 -0300

    feat: add mantine core in the project

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,14 +1,16 @@
+import { MantineProvider } from "@mantine/core";
 import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div className="min-h-screen flex items-center justify-center bg-red-100">
-        <h1 className="text-4xl font-bold text-blue-600">
-          Tailwind funcionando!
-        </h1>
-      </div>
+      <MantineProvider>
+        <div className="min-h-screen flex items-center justify-center bg-red-100">
+          <h1 className="text-4xl font-bold text-blue-600">
+            Tailwind funcionando!
+          </h1>
+        </div>
+      </MantineProvider>
     </>
   );
 }
