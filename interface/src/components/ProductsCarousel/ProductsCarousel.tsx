f346890182b326ba0769cca06336881b73ab0cbe
FabioHenriqueSCC <fhscc7@gmail.com>
2025-06-07 19:59:37 -0300

    fix: adjust in carousel component

--- interface/src/components/ProductsCarousel/ProductsCarousel.tsx
+++ interface/src/components/ProductsCarousel/ProductsCarousel.tsx
@@ -7,22 +7,10 @@ import { ProductCard } from "../ProductCard/ProductCard";
 
 import "@mantine/core/styles/Title.css";
 
-/**
- * ProductsCarousel component for displaying a carousel of product cards.
- * 
- * This component renders a carousel that allows users to scroll through a list of products. It displays a title and a set of product cards using the `ProductCard` component. The carousel supports indicators and custom navigation controls, with options for looping and responsive slide sizing based on screen size.
- * 
- * @param {ProductsCarouselProps} props - The properties passed to the component.
- * @param {string} props.title - The title to be displayed above the carousel.
- * @param {Product[]} props.products - A list of products to display in the carousel. Each product will be passed to the `ProductCard` component for rendering.
- * 
- * @returns {JSX.Element} The rendered carousel component with product cards.
- * 
- * @example
- * // Example usage:
- * // <ProductsCarousel title="Featured Products" products={productList} />
- */
-export const ProductsCarousel = ({ title, products }: ProductsCarouselProps) => {
+export const ProductsCarousel = ({
+  title,
+  products,
+}: ProductsCarouselProps) => {
   return (
     <div className="py-8">
       <Title order={2} className="mb-6 text-[#250D7C]">
@@ -30,7 +18,7 @@ export const ProductsCarousel = ({ title, products }: ProductsCarouselProps) => 
       </Title>
       <Carousel
         withIndicators
-        emblaOptions={{ loop: true, align: "start", slidesToScroll: 5 }}
+        emblaOptions={{ loop: false, align: "start", slidesToScroll: 1 }}
         slideSize={{ base: "100%", sm: "50%", md: "33.333333%" }}
         slideGap={{ base: 0, sm: "md" }}
         styles={{
